4cb60c89495c41d28c874a923825b6c0313ee001
Tsuda12 <frt.120304@gmail.com>
2025-04-16 07:01:10 -0300

    feat: create user entity

--- src/main/java/br/com/tsuda/finance_control_api/entity/User.java
+++ src/main/java/br/com/tsuda/finance_control_api/entity/User.java
@@ -1,4 +1,36 @@
 package br.com.tsuda.finance_control_api.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(UUID id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
